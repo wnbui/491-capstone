beb8094531e6fd522055d2d85196b75149e20902
rbrummett <30569133+rbrummett@users.noreply.github.com>
2025-09-25 14:57:54 -0700

    Create Notes.jsx

---
+++ frontend/pages/Notes.jsx
@@ -0,0 +1,15 @@
+import { Layout } from '../Layout';
+
+export const Notes = () => {
+  return (
+    <Layout pageTitle="NOTES">
+      <div>
+        Note 1 Creator: *** Last Updated ***
+        <br />
+        Note 2 Creator: *** Last Updated ***
+        <br />
+        Note 3 Creator: *** Last Updated ***
+      </div>
+    </Layout>
+  );
+};
